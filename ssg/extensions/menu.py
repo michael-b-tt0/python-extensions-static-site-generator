8f647bd0f9a35478a999f014211821fbe89cf4af
mbman <mbman40@hotmail.com>
2023-02-05 15:14:51 +0000

    part 3

---
+++ ssg/extensions/menu.py
@@ -0,0 +1,25 @@
+from ssg import hooks, parsers
+files = []
+
+
+@hooks.register("collect_files")
+def collect_files(source, site_parsers):
+
+    valid = lambda p : p.isinstance(parsers.ResourceParser)
+    for path in source.rglob("*"):
+        for parser in site_parsers:
+            if valid(parser):
+                if parser.valid_file_ext():
+                    files.append(path)
+
+
+@hooks.register("generate_menu")
+def generate_menu(html,ext):
+    template = '<li><a href="{}{}">{}</a></li>'
+    menu_item = lambda name, ext : template.format(name, ext)
+    menu = "\n".join([menu_item(path.stem, ext) for path in files])
+    return "<ul>\n{}<ul>\n{}".format(menu, html)
+
+    
+
+
