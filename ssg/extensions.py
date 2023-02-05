9dc172eb2d228a0862eaf3d594e67820ae881879
mbman <mbman40@hotmail.com>
2023-02-05 14:07:02 +0000

    part 1

---
+++ ssg/extensions.py
@@ -0,0 +1,22 @@
+import sys, importlib
+from pathlib import Path
+
+def load_module(directory,name):
+
+    sys.path.insert(0, directory)
+    importlib.import_module(name)
+    sys.path.pop(0)
+
+def load_directory(directory):
+    for i in directory.rglob('*.py'):
+        load_module(directory.as_posix(), Path.stem)
+
+def load_bundled():
+    directory = Path("/home/work/Documents/vs_code_projects/python-extensions-static-site-generator/python-extensions-static-site-generator/ssg/extensions").parent
+    
+    load_directory(directory)
+
+
+
+
+print(type(sys.path))
