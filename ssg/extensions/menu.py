b9bc237d18b931efa3d481d7ef9364f566be9a36
mbman <mbman40@hotmail.com>
2023-02-05 15:40:05 +0000

    part3 - 7

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -5,7 +5,7 @@ files = []
 @hooks.register("collect_files")
 def collect_files(source, site_parsers):
 
-    valid = lambda p : p.isinstance(p, parsers.ResourceParser)
+    valid = lambda p :not isinstance(p, parsers.ResourceParser)
     for path in source.rglob("*"):
         for parser in list(filter(valid, site_parsers)):
             if parser.valid_file_ext(path.suffix):
