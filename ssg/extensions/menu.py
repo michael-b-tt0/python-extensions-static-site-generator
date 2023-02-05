3f0a64e45a09f7f19f39d614f70f70e24c2644ad
mbman <mbman40@hotmail.com>
2023-02-05 15:42:22 +0000

    part 3 - 8

--- ssg/extensions/menu.py
+++ ssg/extensions/menu.py
@@ -13,12 +13,13 @@ def collect_files(source, site_parsers):
                 files.append(path)
 
 
+
 @hooks.register("generate_menu")
-def generate_menu(html,ext):
+def generate_menu(html, ext):
     template = '<li><a href="{}{}">{}</a></li>'
-    menu_item = lambda name, ext : template.format(name, ext, name.title())
+    menu_item = lambda name, ext: template.format(name, ext, name.title())
     menu = "\n".join([menu_item(path.stem, ext) for path in files])
-    return "<ul>\n{}<ul>\n{}".format(menu, html)
+    return "<ul>\n{}</ul>\n{}".format(menu, html)
 
 
 
